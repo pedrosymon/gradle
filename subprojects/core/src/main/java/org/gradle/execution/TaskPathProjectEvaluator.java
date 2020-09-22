2bf14a9453270b9812737fb18bffd9824b5f1492
Daz DeBoer <daz@gradle.com>
2020-09-22 17:47:28 -0600

    Always configure intermediate parents when a child project is configured
    When configure-on-demand is enabled, a project is evaluated only when required. In a nested
    project structure, this can mean that a child project is configured without it's parent.
    However, in a nested project structure, the parent project contributes a classloader that is used
    by the child.
    
    These 2 effects combined can result in:
    - The parent project scope class loader is defined before the scope is locked.
      This violates the check enabled by `org.gradle.classloaderscope.strict`.
    - In a composite build, plugins applied to the parent project may not be visible.
    
    This commit fixes this by ensuring that intermediate parent projects are always configured
    before a child project. (The root project is already configured before any children).
    
    Fixes #14552

--- subprojects/core/src/main/java/org/gradle/execution/TaskPathProjectEvaluator.java
+++ subprojects/core/src/main/java/org/gradle/execution/TaskPathProjectEvaluator.java
@@ -33,6 +33,11 @@ public class TaskPathProjectEvaluator implements ProjectConfigurer {
         if (cancellationToken.isCancellationRequested()) {
             throw new BuildCancelledException();
         }
+        // Need to configure intermediate parent projects for configure-on-demand
+        ProjectInternal parentProject = project.getParent();
+        if (parentProject != null && parentProject != project.getRootProject()) {
+            configure(project.getParent());
+        }
         project.evaluate();
     }
 
